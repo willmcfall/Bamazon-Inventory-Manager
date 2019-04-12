d2cb7d98ecaacf96d37c11cd7638e2ab74ca7444
William McFall <wmcfall2@gmail.com>
2019-04-11 23:23:00 -0400

    third commit

--- Bamazon-Customer/Bamazon-Customer.js
+++ Bamazon-Customer/Bamazon-Customer.js
@@ -21,7 +21,7 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
+    // console.log("connected as id " + connection.threadId);
     afterConnection();
 });
 
@@ -51,7 +51,7 @@ function orderRequest() {
             {
                 name: "selection",
                 type: "input",
-                message: "Please select product you would like to buy, by providing the associated item_id:"
+                message: "Please select an item by providing the associated item_id:"
             },
             {
                 name: "quantity",
@@ -63,19 +63,29 @@ function orderRequest() {
 
             connection.query(
                 "SELECT stock_quantity FROM products WHERE item_id=" + answers.selection, function (err, res) {
-                    var integerQuantity = parseInt(res[0].stock_quantity);
-                    var integerAnswer = parseInt(answers.quantity);
-                    console.log(integerQuantity);
-                    console.log(parseInt(integerAnswer));
-                    if (integerAnswer < integerQuantity) {
-                        console.log("Your order is confirmed!");
-                        totalCost(answers,integerAnswer);
-                    }
-                    else if (integerAnswer >= integerQuantity) {
-                        console.log("Inventory is insufficient to process your request! We only have " + integerQuantity + " of this item in stock. Please re-order!");
-                        console.log("---------------------------------------------------------------------------------------")
+                    if(err){
+                        console.log("Please enter a valid id number!");
                         orderRequest();
-                    };
+                    }
+                    else{
+
+                        var integerQuantity = parseInt(res[0].stock_quantity);
+                        var integerAnswer = parseInt(answers.quantity);
+                        // console.log(integerQuantity);
+                        // console.log(parseInt(integerAnswer));
+    
+                        if (integerAnswer < integerQuantity) {
+                            console.log("Your order has been processed!");
+                            totalCost(answers,integerAnswer);
+                            updateTable(integerQuantity, integerAnswer, answers);
+                        }
+                        else if (integerAnswer >= integerQuantity) {
+                            console.log("Inventory is insufficient to process your request! We only have " + integerQuantity + " of this item in stock. Please re-order!");
+                            console.log("------------------------------------------------------------------------")
+                            orderRequest();
+                        };
+
+                    }
 
                 });
         });
@@ -87,7 +97,11 @@ function totalCost(answers, integerAnswer){
     connection.query(
         "SELECT price FROM products WHERE item_id=" + answers.selection, function (err, res) {
             var floatPrice = parseFloat(res[0].price);
-            console.log("The total cost of your order is $" + (floatPrice * integerAnswer) + ".");
+            var totalCost = floatPrice * integerAnswer;
+            totalCost = Math.floor(totalCost * 100) / 100;
+            totalCost = totalCost.toFixed(2);
+
+            console.log("The total cost of your order is $" + totalCost + ".");
 
             inquirer
         .prompt([
@@ -102,7 +116,9 @@ function totalCost(answers, integerAnswer){
         .then(answers => {
             
             if(answers.reorder == "Yes"){
-            console.log("---------------------------------------------------------------------------------------");
+            console.log("------------------------------------------------------------------");
+            console.log("--------------------------NEXT ORDER------------------------------");
+            console.log("------------------------------------------------------------------");
                afterConnection();
             }
             else if (answers.reorder == "No"){
@@ -116,145 +132,23 @@ function totalCost(answers, integerAnswer){
 };
 
 
+function updateTable(integerQuantity, integerAnswer, answers){
 
-// Check whether the quantity requested can be accommodated by the inventory;
-
-// IF YES
-// process the request
-// update the database
-// provide the user with the total bill of their purchase
-
-
-// IF NO
-// alert user
-// prevent processing of request
-// do not update database
-
-
-
-
-// creates empty database structure (id, item name, item category, ask price, bid price)
-
+    var query = connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+          {
+            stock_quantity: (integerQuantity - integerAnswer) 
+          },
+          {
+            item_id: answers.selection
+          }
+        ],
+        function(err, res) {
+        //   console.log("Updated successfully");
+        }
+      );    
+};
 
 
-
-
-
-// function afterConnection() {
-
-//    inquirer
-//        .prompt([
-//            {
-//                name: "action",
-//                type: "input",
-//                message: "Would you like to [Post] or [Bid] on an auction?"
-//            }
-//        ])
-//        .then(answers => {
-
-//            if (answers.action == "post") {
-//                ask();
-//            }
-//            else if (answers.action == "bid") {
-//                bid();
-//            }
-
-//        });
-
-// };
-
-
-
-// function ask() {
-//    console.log("ask function worked");
-
-//    inquirer
-//        .prompt([
-//            {
-//                name: "item",
-//                type: "input",
-//                message: "Enter name of item to be auctioned:"
-//            },
-//            {
-//                name: "itemcategory",
-//                type: "list",
-//                message: "Select item category",
-//                choices: ["Electronics", "Apparel", "Other"]
-//            },
-//            {
-//                name: "askprice",
-//                type: "input",
-//                message: "Enter ask price for item:"
-//            },
-//        ])
-//        .then(answers => {
-//            var query = connection.query(
-//                "INSERT INTO greatbay SET ?",
-//                {
-//                    item: answers.item,
-//                    itemcategory: answers.itemcategory,
-//                    askprice: answers.askprice
-//                },
-//                function (err, res) {
-//                    console.log(res.affectedRows + " product inserted!\n");
-//                }
-//            );
-
-//        });
-// };
-
-
-
-// function bid() {
-//    console.log("bid function worked");
-
-//    var itemChoices = []
-//    connection.query(
-//        "SELECT item FROM greatbay", function (err, res) {
-//            console.log(res.affectedRows + " product inserted!\n");
-//            //console.log(res)
-//            for (var i = 0; i < res.length; i++) {
-//                itemChoices.push(res[i].item)
-//            }
-//            withinAsk()
-//        })
-//    console.log(itemChoices)
-
-//    function withinAsk() {
-
-//        inquirer.prompt([
-//            {
-//                name: 'item',
-//                type: 'list',
-//                message: 'Select item for bidding',
-//                choices: itemChoices
-//            },
-//            {
-//                name: 'bidprice',
-//                type: 'input',
-//                message: 'Enter price you would like to bid on this item'
-//            }
-//        ]).then(answers => {
-//            function updateProduct() {
-//                console.log('updating')
-
-//                var query = connection.query('UPDATE greatbay SET ? WHERE ?', [
-//                    {
-//                        bidprice: answers.bidprice
-//                    },
-//                    {
-//                        item: answers.item
-//                    }
-//                ], function (err, res) {
-//                    console.log(res);
-//                    checkProduct();
-
-//                })
-//            }
-//            updateProduct()
-//        })
-
-
-
-//    }
 
